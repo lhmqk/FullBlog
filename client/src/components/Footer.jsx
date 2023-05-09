887b7ffc782314d167546281045a5f6b8d2f08d0
lhmqk <jamethewood05@gmail.com>
2023-05-10 01:17:40 +0700

    style navbar, footer, home

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Logo from "../img/logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>Copyright © 2023 lh.mqk</span>
+    </footer>
+  );
 };
 
 export default Footer;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../img/logo.png";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="logo">logo</div>
-        <div className="links">links</div>
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=uni">
+            <h6>Uni</h6>
+          </Link>
+          <Link className="link" to="/?cat=billiard">
+            <h6>Billiard</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>Food</h6>
+          </Link>
+          <span>John</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   );
