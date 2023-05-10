8367481afc97cda16e580d1e711cb49686fc17bc
lhmqk <jamethewood05@gmail.com>
2023-05-10 11:53:23 +0700

    implement register, login

--- api/db.js
+++ api/db.js
@@ -1,6 +1,6 @@
-import mysql from "mysql";
+import mysql2 from "mysql2";
 
-export const db = mysql.createConnection({
+export const db = mysql2.createConnection({
   host: "localhost",
   user: "root",
   password: "TueVan191105",
