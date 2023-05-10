c880f09f2c04a993025713b9a3815d3c9aedb676
lhmqk <jamethewood05@gmail.com>
2023-05-10 10:49:53 +0700

    init routes, controllers

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(8800, () => {
   console.log("Connected to backend");
